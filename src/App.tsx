a96a2d0c0bb9bcd490b4001b3ee7d080595b8dfe
Tarlan <144718247+TarlanNaz@users.noreply.github.com>
2024-12-08 18:11:20 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
+
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage';
 import AdminPage from './pages/admin/AdminPage';
 import TopicsPage from './pages/topic/TopicsPage';
